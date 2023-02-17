7f58352d6c9e24128157b43e663f6a293364e5d6
dina <dinaelhadidy966@gmail.com>
2023-02-17 07:38:40 -0500

    6 space fix

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,9 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digit numbers of base 10 starting from 0,
- * 	  followed by a new line.
- * 	  You can only use the putchar function
+ * main - prints all single digit numbers of base 10 starting from 0
  * Return: Always 0 (Success)
  */
 int main(void)
