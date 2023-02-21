390d56c2c94290b250889ae496dfdf09eddd2dfc
dina <dinaelhadidy966@gmail.com>
2023-02-21 10:18:31 -0500

    0 to 9

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - function that prints 10 times the alphabet,
+ * in lowercase, followed by a new line
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while(i < 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}
