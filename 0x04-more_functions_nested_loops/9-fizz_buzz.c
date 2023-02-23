8db4e0fbea2caa2211d86d0f1d20ec43177df50f
Dina El-hadidy <81911556+Dina170@users.noreply.github.com>
2023-02-23 16:10:40 +0200

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,22 +9,31 @@
  */
 int main(void)
 {
-	int num;
+	int x;
 
-	for (num = 1; num <= 100; num++)
+	for (x = 1; x <= 100; x++)
 	{
-		if ((num % 3) == 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (x % 3 == 0)
+		{
 			printf("Fizz");
-
-		if ((num % 5) == 0)
+		}
+		else if (x % 5 == 0)
+		{
 			printf("Buzz");
-
+		}
 		else
-			printf("%d", num);
+		{
+			printf("%d", x);
+		}
 
-		if (num == 100)
-			continue;
-		printf(" ");
+		if (x != 100)
+		{
+			printf(" ");
+		}
 	}
 
 	printf("\n");
