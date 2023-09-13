2f5340ba4d1e356b8c8a8055d59b1c57828710a1
dina <dinaelhadidy966@gmail.com>
2023-09-13 06:50:57 -0400

    Fix betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
-  * linear_search - Searches for a value in an array of integers using the Linear search algorithm
+  * linear_search - Searches for a value in an array of integers
+  * using the Linear search algorithm
   * @array: pointer to the first element of the array to search in
   * @size: the number of elements in array.
   * @value: the value to search for
--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
-  * binary_search - Searches for a value in a sorted array of integers using the Binary search algorithm
+  * binary_search - Searches for a value in a sorted array of integers
+  * using the Binary search algorithm
   * @array: pointer to the first element of the array to search in
   * @size: the number of elements in array.
   * @value: the value to search for
@@ -13,7 +14,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t i, low = 0, high = size - 1;
 
-	if (array == NULL)
+	if (!array)
 		return (-1);
 	while (low <= high)
 	{
